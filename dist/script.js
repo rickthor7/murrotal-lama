da24848cc14a4b3f7fdf3bab931332890d8b4644
rickthor7 <96973185+rickthor7@users.noreply.github.com>
2022-09-14 19:18:57 +0700

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -48,9 +48,9 @@ const Nav = props => {
     React.createElement(React.Fragment, null, /*#__PURE__*/
     React.createElement("nav", { id: "navbar" }, /*#__PURE__*/
     React.createElement("div", { className: "nav-wrapper" }, /*#__PURE__*/
-    React.createElement("p", { className: "brand" }, "yago", /*#__PURE__*/
+    React.createElement("p", { className: "brand" }, "murrotal", /*#__PURE__*/
 
-    React.createElement("strong", null, "est\xE9vez")), /*#__PURE__*/
+    React.createElement("strong", null, "30Juz")), /*#__PURE__*/
 
     React.createElement("a", {
       onClick: props.toggleMenu,
@@ -78,14 +78,14 @@ const Header = props => {
     React.createElement("div", { className: "moon" }), /*#__PURE__*/
     React.createElement("div", { className: "container" }, /*#__PURE__*/
     React.createElement("h1", null, /*#__PURE__*/
-    React.createElement("span", { className: "line" }, "I do"), /*#__PURE__*/
-    React.createElement("span", { className: "line" }, "graphic design"), /*#__PURE__*/
+    React.createElement("span", { className: "line" }, "Website"), /*#__PURE__*/
+    React.createElement("span", { className: "line" }, "murrotal alquran"), /*#__PURE__*/
     React.createElement("span", { className: "line" }, /*#__PURE__*/
-    React.createElement("span", { className: "color" }, "&"), " code.")), /*#__PURE__*/
+    React.createElement("span", { className: "color" }, "30"), " juz.")), /*#__PURE__*/
 
 
     React.createElement("div", { className: "buttons" }, /*#__PURE__*/
-    React.createElement("a", { href: "#projects" }, "my portfolio"), /*#__PURE__*/
+    React.createElement("a", { href: "#projects" }, "go to murrotal"), /*#__PURE__*/
     React.createElement("a", { href: "#contact", className: "cta" }, "get in touch")))));
 
 
@@ -106,16 +106,16 @@ const About = props => {
     React.createElement("div", { className: "wrapper" }, /*#__PURE__*/
     React.createElement("article", null, /*#__PURE__*/
     React.createElement("div", { className: "title" }, /*#__PURE__*/
-    React.createElement("h3", null, "Who's this guy?"), /*#__PURE__*/
+    React.createElement("h3", null, "Tentang Alquran Online"), /*#__PURE__*/
     React.createElement("p", { className: "separator" })), /*#__PURE__*/
 
     React.createElement("div", { className: "desc full" }, /*#__PURE__*/
-    React.createElement("h4", { className: "subtitle" }, "My name is Yago."), /*#__PURE__*/
-    React.createElement("p", null, "I am a web developer and UX designer based in the beautiful west coast of Spain, Galicia."), /*#__PURE__*/
+    React.createElement("h4", { className: "subtitle" }, "Made By M.thoriq."), /*#__PURE__*/
+    React.createElement("p", null, "Website alquran ini dibuat bertujuan khususnya bagi author sendiri untuk memudahkan murajaah Alquran."), /*#__PURE__*/
 
 
 
-    React.createElement("p", null, "I really enjoy solving problems as well as making things pretty and easy to use. I can't stop learning new things; the more, the better. I also love photography, a hobby I'm taking along since the good old film cameras. Oh, and rice with milk; I have a passion for rice with milk!")), /*#__PURE__*/
+    React.createElement("p", null, "Semoga dengan adanya website ini, kita dimudahkan untuk menghafal/murajaah alquran agar semakin lancar&berkah")), /*#__PURE__*/
 
 
 
@@ -123,24 +123,24 @@ const About = props => {
 
 
     React.createElement("div", { className: "title" }, /*#__PURE__*/
-    React.createElement("h3", null, "What does he do?"), /*#__PURE__*/
+    React.createElement("h3", null, "About Author"), /*#__PURE__*/
     React.createElement("p", { className: "separator" })), /*#__PURE__*/
 
     React.createElement("div", { className: "desc" }, /*#__PURE__*/
-    React.createElement("h4", { className: "subtitle" }, "I'm a programmer."), /*#__PURE__*/
-    React.createElement("p", null, "For the front-end I usually work with Javascript, either standalone or including popular frameworks like ReactJS and VueJS. I also make the web pretty by using Sass, CSS and, whenever needed, any of their friends: Bootstrap, Bulma, etc."), /*#__PURE__*/
+    React.createElement("h4", { className: "subtitle" }, "I'm a web developer."), /*#__PURE__*/
+    React.createElement("p", null, "Saya memulai project ini mulai dari tahun 2022 bulan agustus awal karena terinspirasi dari buku audiosonic yang menjelaskan tentang dahsyatnya ingatan pendengaran"), /*#__PURE__*/
 
 
 
 
-    React.createElement("p", null, "For the back-end I also work with Javascript (NodeJS, Express, MongoDB, etc). But, of course, whenever the project requires PHP, I do PHP as well (Wordpress, Laravel, etc).")), /*#__PURE__*/
+    React.createElement("p", null, "Selebihnya tentang saya bisa berkunjung di instagram @iam.rickthor7")), /*#__PURE__*/
 
 
 
 
     React.createElement("div", { className: "desc" }, /*#__PURE__*/
-    React.createElement("h4", { className: "subtitle" }, "Also a designer."), /*#__PURE__*/
-    React.createElement("p", null, "I feel comfortable working with many Adobe products. Photoshop, Illustrator, InDesign, Lightroom or Xd are some kind of industry standards and I love working with them. I'm not limited to them, though: Gimp, Inkscape or Figma are also very valid applications that I've been working with."), /*#__PURE__*/
+    React.createElement("h4", { className: "subtitle" }, "My Socmed."), /*#__PURE__*/
+    React.createElement("p", null, "Instagram@iam.rickthor7 whatsapp+6281234397578 twitter@rickthor7."), /*#__PURE__*/
 
 
 
