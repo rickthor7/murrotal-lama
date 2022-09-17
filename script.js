ca1598428cdba7e31d093f28ac75b90cf5c1e779
rickthor7 <96973185+rickthor7@users.noreply.github.com>
2022-09-17 14:37:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -299,160 +299,28 @@ const Projects = props => {
 
 
     React.createElement(Project, {
-      title: "URL Shortener Microservice.",
-      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/Cortala.jpg',
-      tech: "js node css",
-      link: "https://cortala.glitch.me/example",
-      repo: "https://github.com/yagoestevez/cortala" }, /*#__PURE__*/
+      title: "Nabil Ar-Rifai.",
+      img: 'https://raw.githubusercontent.com/rickthor7/murrotal/800805ec25122d12ba30f22ecb3fd7b7158d9a65/src/imam/nabilarrifai.jpg',
+      tech: " ",
+      link: "https://www.rickthor7.my.id/murrotal_nabil",
+      repo: "https://www.rickthor7.my.id/murrotal_nabil" }, /*#__PURE__*/
 
-    React.createElement("small", null, "Built using Node, Express, CSS + Bootstrap and Pug."), /*#__PURE__*/
-    React.createElement("p", null, "A microservice which takes a raw URL and makes it a short link to make it easy to sent it through the Internet.")), /*#__PURE__*/
+    React.createElement("small", null, "Nabil Bin Sheikh Abdul Rahim Rifai lahir pada tahun 1977 di Jeddah, Arab Saudi.dan tahun 1994, Syekh Nabil Ar Rifai mulai memimpin sholat Tarawih sebagai Imam di Masjid Ramadhan Baghdadi."), /*#__PURE__*/
+    React.createElement("p", null, "Nabil Ar Rifai juga mendapat pujian sebagai lulusan Fakultas Teknik Universitas Raja Fahd Abdul Aziz.")), /*#__PURE__*/
 
 
 
 
     React.createElement(Project, {
       title: "Exercise Tracker.",
-      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/ExerciseTracker.jpg',
-      tech: "js vue node css",
-      link: "https://yagoestevez-exercise-tracker.glitch.me",
-      repo: "https://github.com/yagoestevez/exercise-tracker" }, /*#__PURE__*/
+      img: 'https://raw.githubusercontent.com/rickthor7/murrotal/800805ec25122d12ba30f22ecb3fd7b7158d9a65/src/imam/AbdullahAlMathrud.jpg',
+      tech: " ",
+      link: "https://www.rickthor7.my.id/murrotal_matrud",
+      repo: "https://www.rickthor7.my.id/murrotal_matrud" }, /*#__PURE__*/
 
-    React.createElement("small", null, "Built using Node, Express, VueJS + Vue Router and CSS + Bootstrap."), /*#__PURE__*/
-    React.createElement("p", null, "A service which lets the users save their daily exercise (or anything) into a log for them to check it later.")), /*#__PURE__*/
+    React.createElement("small", null, "Abdullah bin Muhammad Al-Mathrud.  imam dan khatib di Masjid jami’ الأمير بندر بن عبد العزيز آل سعود, bacaanya tenang dan khusyu’ dengan suara enak."), /*#__PURE__*/
+    React.createElement("p", null, "beliau adalah atlet, aktor, juga penyanyi.Beliau ahli dalam sepakbola, tenis dan bola voli. Ketika awal mengaji, gurunya berkata; “Sesungguhnya itu adalah lebih baik daripada bernyanyi” sehingga ia termotivasi dan sekarang menjadi imam.")), /*#__PURE__*/
 
-
-
-
-    React.createElement(Project, {
-      title: "Bar Chart.",
-      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/BarChart.jpg',
-      tech: "js d3 css",
-      link: "https://codepen.io/yagoestevez/full/wxjmrB/",
-      repo: "https://github.com/yagoestevez/fcc-barchart" }, /*#__PURE__*/
-
-    React.createElement("small", null, "Built using CSS, JS and D3.js."), /*#__PURE__*/
-    React.createElement("p", null, "A bar chart representing the evolution of the US GDP.")), /*#__PURE__*/
-
-    React.createElement(Project, {
-      title: "Treemap Diagram.",
-      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/TreemapDiagram.jpg',
-      tech: "js d3 css",
-      link: "https://codepen.io/yagoestevez/full/bjZygz/",
-      repo: "https://github.com/yagoestevez/fcc-treemap-diagram" }, /*#__PURE__*/
-
-    React.createElement("small", null, "Built using CSS, JS and D3.js."), /*#__PURE__*/
-    React.createElement("p", null, "A treemap diagram representing groups of pledges and sales from a particular category.")), /*#__PURE__*/
-
-
-
-    React.createElement(Project, {
-      title: "Choropleth Map.",
-      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/ChoroplethMap.jpg',
-      tech: "js d3 css",
-      link: "https://codepen.io/yagoestevez/full/NBeaWK/",
-      repo: "https://github.com/yagoestevez/fcc-choropleth-map" }, /*#__PURE__*/
-
-    React.createElement("small", null, "Built using CSS, JS and D3.js."), /*#__PURE__*/
-    React.createElement("p", null, "A choropleth map representing the educational attainment by county in the U.S.")), /*#__PURE__*/
-
-    React.createElement(Project, {
-      title: "Random Quoting Machine.",
-      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/QuotingMachine.jpg',
-      tech: "js vue css",
-      link: "https://codepen.io/yagoestevez/full/bxgEyd/",
-      repo: "https://github.com/yagoestevez/random-quoting-machine" }, /*#__PURE__*/
-
-    React.createElement("small", null, "Built using VueJS, Axios and CSS + Bootstrap."), /*#__PURE__*/
-    React.createElement("p", null, "A random quoting app which retrieves pictures and quotes from two different APIs.")), /*#__PURE__*/
-
-    React.createElement(Project, {
-      title: "Calculator.",
-      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/Calculator.jpg',
-      tech: "js react css",
-      link: "https://codepen.io/yagoestevez/full/ERVONM/",
-      repo: "https://github.com/yagoestevez/the-calcoolator" }, /*#__PURE__*/
-
-    React.createElement("small", null, "Built using React and CSS."), /*#__PURE__*/
-    React.createElement("p", null, "A project on which I built a virtual calculator with its usual features.")), /*#__PURE__*/
-
-    React.createElement(Project, {
-      title: "Pomodoro Timer.",
-      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/TomateTimer.jpg',
-      tech: "js react css",
-      link: "https://codepen.io/yagoestevez/full/dqJGVa",
-      repo: "https://github.com/yagoestevez/tomate-time" }, /*#__PURE__*/
-
-    React.createElement("small", null, "Built using React, CSS and lots of SVG."), /*#__PURE__*/
-    React.createElement("p", null, "A cute and animated Pomodoro clock to help the users improve their productivity.")), /*#__PURE__*/
-
-    React.createElement(Project, {
-      title: "TicTacToe Game.",
-      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/TicTacToe.jpg',
-      tech: "js react css",
-      link: "https://codepen.io/yagoestevez/full/KRevzB/",
-      repo: "https://github.com/yagoestevez/TicTacToe" }, /*#__PURE__*/
-
-    React.createElement("small", null, "Built using React, CSS and SVG."), /*#__PURE__*/
-    React.createElement("p", null, "A TicTacToe game with a basic AI algorithm made for the legacy front-end projects on FreeCodeCamp.")), /*#__PURE__*/
-
-
-
-
-    React.createElement(Project, {
-      title: "Twitch Clone.",
-      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/Twitch.jpg',
-      tech: "js vue sass",
-      link: "https://codepen.io/yagoestevez/full/xjwVPq/" }, /*#__PURE__*/
-
-
-    React.createElement("small", null, "Built using VueJS, Axios and Scss."), /*#__PURE__*/
-    React.createElement("p", null, "Simple Twitch clone using the new Twitch Helix API and made for the legacy front-end projects on FreeCodeCamp.")), /*#__PURE__*/
-
-
-
-
-    React.createElement(Project, {
-      title: "Weather App.",
-      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/WeatherApp.jpg',
-      tech: "js vue css",
-      link: "https://codepen.io/yagoestevez/full/zWbGmZ/" }, /*#__PURE__*/
-
-
-    React.createElement("small", null, "Built using VueJS, Axios and CSS."), /*#__PURE__*/
-    React.createElement("p", null, "App that present the users with the current local weather for their location.")), /*#__PURE__*/
-
-    React.createElement(Project, {
-      title: "Tribute.",
-      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/Tribute.jpg',
-      tech: "js sass",
-      link: "https://codepen.io/yagoestevez/full/aaaOxL/",
-      repo: "https://github.com/yagoestevez/tribute-to-aaron-swartz" }, /*#__PURE__*/
-
-    React.createElement("small", null, "Built using vanilla Javascript and Scss."), /*#__PURE__*/
-    React.createElement("p", null, "Tribute page to Aaron Swartz, a static website built with SCSS and plain Javascript.")), /*#__PURE__*/
-
-
-
-    React.createElement(Project, {
-      title: "Survey Form.",
-      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/SurveyForm.jpg',
-      tech: "js sass",
-      link: "https://codepen.io/yagoestevez/full/bjVgjX/",
-      repo: "https://github.com/yagoestevez/membership-form" }, /*#__PURE__*/
-
-    React.createElement("small", null, "Built using vanilla Javascript and Scss."), /*#__PURE__*/
-    React.createElement("p", null, "An animated survey form.")), /*#__PURE__*/
-
-    React.createElement(Project, {
-      title: "Landing Page Example.",
-      img: 'https://raw.githubusercontent.com/yagoestevez/fcc-portfolio/master/src/Components/Projects/Images/LandingPage.jpg',
-      tech: "js sass",
-      link: "https://codepen.io/yagoestevez/full/pOBLeK/",
-      repo: "https://github.com/yagoestevez/tomate-timer-landing-page" }, /*#__PURE__*/
-
-    React.createElement("small", null, "Built using vanilla Javascript and Scss."), /*#__PURE__*/
-    React.createElement("p", null, "A landing page for the Pomodoro Timer I made (shown previously in this section).")), /*#__PURE__*/
 
     React.createElement(Project, {
       title: "Technical Documentation Page.",
@@ -484,16 +352,16 @@ const Contact = props => {
     React.createElement("div", { className: "container" }, /*#__PURE__*/
     React.createElement("div", { className: "heading-wrapper" }, /*#__PURE__*/
     React.createElement("div", { className: "heading" }, /*#__PURE__*/
-    React.createElement("p", { className: "title" }, "Want to ", /*#__PURE__*/
-    React.createElement("br", null), "contact me?"), /*#__PURE__*/
+    React.createElement("p", { className: "title" }, "Ingin requets? ", /*#__PURE__*/
+    React.createElement("br", null), "Hubungi Saya?"), /*#__PURE__*/
 
 
     React.createElement("p", { className: "separator" }), /*#__PURE__*/
-    React.createElement("p", { className: "subtitle" }, "Please, use the form below or send an email to ",
+    React.createElement("p", { className: "subtitle" }, "Atau dapat menekan link di garis 3 pojok kanan",
     '', /*#__PURE__*/
     React.createElement("span", { className: "mail" }, "web", /*#__PURE__*/
 
-    React.createElement("i", { className: "fas fa-at at" }), "yagoestevez", /*#__PURE__*/
+    React.createElement("i", { className: "fas fa-at at" }), "rickthorid@gmail", /*#__PURE__*/
 
     React.createElement("i", { className: "fas fa-circle dot" }), "com"), ":")), /*#__PURE__*/
 
@@ -525,7 +393,7 @@ const Footer = props => {
     React.createElement("footer", null, /*#__PURE__*/
     React.createElement("div", { className: "wrapper" }, /*#__PURE__*/
     React.createElement("h3", null, "THANKS FOR VISITING"), /*#__PURE__*/
-    React.createElement("p", null, "\xA9 ", new Date().getFullYear(), " Yago Est\xE9vez."), /*#__PURE__*/
+    React.createElement("p", null, "\xA9 ", new Date().getFullYear(), " M. Thoriq Dhiya Ul Haq"), /*#__PURE__*/
     React.createElement(SocialLinks, null))));
 
 
@@ -543,32 +411,32 @@ const SocialLinks = props => {
   return /*#__PURE__*/(
     React.createElement("div", { className: "social" }, /*#__PURE__*/
     React.createElement("a", {
-      href: "https://twitter.com/yagoestevez",
+      href: "https://instagram.com/iam.rickthor7",
       target: "_blank",
       rel: "noopener noreferrer",
-      title: "Link to author's Twitter profile" },
+      title: "Link to author's Instagram profile" },
 
     ' ', /*#__PURE__*/
-    React.createElement("i", { className: "fab fa-twitter" })), /*#__PURE__*/
+    React.createElement("i", { className: "fab fa-instagram" })), /*#__PURE__*/
 
     React.createElement("a", {
       id: "profile-link",
-      href: "https://github.com/yagoestevez",
+      href: "https://wa.me/6281234397578",
       target: "_blank",
       rel: "noopener noreferrer",
-      title: "Link to author's GitHub Profile" },
+      title: "Link to author's whatsapp" },
 
     ' ', /*#__PURE__*/
-    React.createElement("i", { className: "fab fa-github" })), /*#__PURE__*/
+    React.createElement("i", { className: "fab fa-whatsapp" })), /*#__PURE__*/
 
     React.createElement("a", {
-      href: "https://codepen.io/yagoestevez",
+      href: "https://www.rickthor7.my.id",
       target: "_blank",
       rel: "noopener noreferrer",
-      title: "Link to author's Codepen Profile" },
+      title: "Link to author's Website" },
 
     ' ', /*#__PURE__*/
-    React.createElement("i", { className: "fab fa-codepen" }))));
+    React.createElement("i", { className: "fab fa-globe" }))));
 
 
 
